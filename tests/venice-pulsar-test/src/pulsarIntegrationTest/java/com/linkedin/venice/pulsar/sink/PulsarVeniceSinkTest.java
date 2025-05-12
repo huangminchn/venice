801bc85a19e0e190c1883e24483e793387db1561
Min Huang <mnhuang@linkedin.com>
2025-05-12 13:10:36 -0700

    here we go

--- tests/venice-pulsar-test/src/pulsarIntegrationTest/java/com/linkedin/venice/pulsar/sink/PulsarVeniceSinkTest.java
+++ tests/venice-pulsar-test/src/pulsarIntegrationTest/java/com/linkedin/venice/pulsar/sink/PulsarVeniceSinkTest.java
@@ -221,7 +221,7 @@ public class PulsarVeniceSinkTest {
       String name = "name" + i;
       int expectedAge = i;
 
-      Awaitility.await().atMost(30, TimeUnit.SECONDS).untilAsserted(() -> {
+      Awaitility.await().atMost(60, TimeUnit.SECONDS).untilAsserted(() -> {
         // StdErr will have some noise like
         // "StatusLogger Log4j2 could not find a logging implementation."
         ExecResult res = execByService(
@@ -230,6 +230,7 @@ public class PulsarVeniceSinkTest {
             "-c",
             "java -jar /opt/venice/bin/venice-thin-client-all.jar " + storeName + " " + name + " " + veniceRouterUrl
                 + " " + "false" + " " + "\"\"");
+        LOGGER.info("[DEBUGDEBUG] response for key {}: {}", name, res.getStdout());
         assertTrue(res.getStdout().contains("key=" + name));
         assertFalse(res.getStdout().contains("value=null"));
         assertTrue(res.getStdout().contains("value=" + "{\"age\": " + expectedAge + ", \"name\": \"" + name + "\"}"));
